70a3ce0b67d6b543181beda7852606a6e5597d11
choudel <chouaibh@gmail.com>
2022-06-02 21:51:00 +0100

    addBack, addAt, peekFront, peekBack, get , indexOf

--- src/linkedList_v2.ts
+++ src/linkedList_v2.ts
@@ -5,8 +5,16 @@
      tail: LinkedListNode<T>
      size: number
  }
+ interface CompareFunction<T>{
+     (a:T , b:T):number
+ }
+ interface EqualsFunction<T> {
+    (a: T, b: T): boolean
+  }
 
-
+ const defaultEquals=<T>(a:T,b:T):boolean=>{
+     return a === b
+ }
  class LinkedList<T> implements Iterable<T>{
      private list: List<T>|undefined
      constructor(){
@@ -39,6 +47,77 @@
          }
 
      }
+     addBack(val:T):void{
+         const NewNode = new LinkedListNode(val)
+         if (this.list){
+             this.list.tail.next=NewNode
+             NewNode.prev=this.list.tail
+             this.list.tail=NewNode
+             this.list.size += 1
+         }else{
+            this.list={
+                head: NewNode,
+                tail: NewNode,
+                size: 1,
+            }
+         }
+    }
+    addAt(i:number, val:T):void{
+        if (i===0){
+            this.addFront(val)
+            return
+        }
+        if(i==this.size()){
+            this.addBack(val)
+        }
+        if (i<0 ||i>= this.size()||!this.list){
+            throw new Error('not permitted')
+        }
+        let current = this.list.head
+        for (let j=0; j<i-1;j++){
+            current= current.next!
+        }
+        const NewNode= new LinkedListNode(val)
+        current.next!.prev=NewNode
+        NewNode.next=current.next
+        NewNode.prev=current
+        current.next=NewNode
+        this.list.size += 1 
+    }
+    peekFront():T{
+        if (!this.list) throw new Error('empty field')
+        return this.list.head.val
+    }
+    peekBack():T{
+        if (!this.list) throw new Error('empty field')
+        return this.list.tail.val
+    }
+
+    get (i:number):T{
+        if (i<0 ||i>= this.size()||!this.list){
+            throw new Error('not permitted')
+        }
+        let current = this.list.head
+        for (let j=0;j<i-1;j++){
+            current=current.next!
+        }
+        return current.val
+    }
+    indexOf(value:T, equalsFunction?:EqualsFunction<T>):number{
+        if (!this.list) return -1
+        const equalsF = equalsFunction || defaultEquals
+
+        let i=0
+        let current = this.list.head
+        while(!equalsF(current.val,value)){
+            current=current.next!
+            i += 1
+
+        }
+
+        return i
+    }
+    
  }
 
 
